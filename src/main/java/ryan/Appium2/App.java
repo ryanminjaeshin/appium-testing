ee2de4648f67bd60437eec5f82d5af2df0b54d21
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-18 18:51:54 +0900

    first commoit

---
+++ src/main/java/ryan/Appium2/App.java
@@ -0,0 +1,13 @@
+package ryan.Appium2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
