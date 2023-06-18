2212dd009c59648b0ee18cab4cb09ff1b0885aa4
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-18 19:54:51 +0900

    add setName,setApp

--- src/test/java/ryan/Appium2/AppiumBasics.java
+++ src/test/java/ryan/Appium2/AppiumBasics.java
@@ -6,14 +6,20 @@ import java.net.URL;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class AppiumBasics {
 	
 	@Test
 	public void AppiumTest() throws MalformedURLException {
 		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("ryanphone");
+		options.setApp("//Users//ryanshin//eclipse-workspace//Appium2//src//test//java//resources//ApiDemos-debug.apk");
+		
 		// first argument => Appium server port
-		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), null);
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+		driver.quit();
 	}
 
 }
